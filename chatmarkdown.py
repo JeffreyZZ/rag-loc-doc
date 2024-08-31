5390d4abc7bb34aeb251b28e3cfe86309c8dc300
Jeffrey Zhou <jingzhou@microsoft.com>
2024-08-30 20:57:03 -0700

    Enable hybrid search

--- chatmarkdown.py
+++ chatmarkdown.py
@@ -9,11 +9,16 @@ from langchain.schema.runnable import RunnablePassthrough
 from langchain.prompts import PromptTemplate
 from langchain_community.vectorstores.utils import filter_complex_metadata
 from langchain_ollama import OllamaEmbeddings
+from langchain.retrievers import ContextualCompressionRetriever
+from langchain.retrievers.document_compressors import FlashrankRerank
+from langchain_community.retrievers import BM25Retriever
+from langchain.retrievers import EnsembleRetriever
 
 class ChatMarkdown:
     vector_store = None
     retriever = None
     chain = None
+    keyword_retriever = None
 
     def __init__(self):
         self.model = ChatOllama(model="mistral")
@@ -61,22 +66,49 @@ class ChatMarkdown:
         # Check if all_chunks is an empty list
         if not all_chunks:
             return False
-        
-        #============
-        # Embedding
-        #============
+
+        #===========================
+        # Embedding: vector search
+        #===========================
         # using local running mxbai-embed-large embedding model with Ollama, so needs to install Ollama
         # and pull mxbai-embed-large model.  
+        # mxbai-embed-large: https://www.mixedbread.ai/docs/embeddings/mxbai-embed-large-v1
         embeddings = OllamaEmbeddings(
             model="mxbai-embed-large",
         )
         self.vector_store =  Chroma.from_documents(documents=all_chunks, embedding=embeddings) # FastEmbedEmbeddings() is faster one for local debugging purpose
-        self.retriever =  self.vector_store.as_retriever(
+        # TODO: output retriever result for debugging
+        retriever =  self.vector_store.as_retriever(
             search_type="similarity_score_threshold",
             search_kwargs={
                 "k": 6,
                 "score_threshold": 0.3,
             },
+        )
+
+        #=======================
+        # Keyword search: BM25
+        #=======================
+        bm25_retriever = BM25Retriever.from_documents(all_chunks)
+        bm25_retriever.k =  6  # Retrieve top 2 results
+        self.keyword_retriever = bm25_retriever
+
+        #===================================
+        # Hybrid serarch: vector + keyword
+        #===================================
+        ensemble_retriever = EnsembleRetriever(retrievers=[bm25_retriever, retriever],
+                                               weights=[0.4, 0.6])
+       
+
+        #============
+        # Reranking 
+        #============
+        # FlashRank: https://github.com/PrithivirajDamodaran/FlashRank
+        # TODO: output reranking result for debugging
+        compressor = FlashrankRerank()
+        self.retriever = ContextualCompressionRetriever(
+            base_compressor=compressor, 
+            base_retriever=ensemble_retriever
         )
 
         self.chain = ({"context": self.retriever, "question": RunnablePassthrough()}
@@ -95,7 +127,10 @@ class ChatMarkdown:
             chunks += f"""------[{score:.4f}]-----------------------------------------------------------
             {doc.metadata}
             {doc.page_content}
-            """      
+            """     
+
+        keyword_results = self.keyword_retriever.invoke(query)
+
         return self.chain.invoke(query) + '\n' + chunks
 
     def clear(self):
